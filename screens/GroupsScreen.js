b555d3d3040f6f1ff2fa265d2e97a7637ec7ecb0
Sriram Senthil <sriram.senthilkr@gmail.com>
2022-12-19 20:39:00 +0800

    Added Icons and more css

--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -1,22 +1,31 @@
-import { View, Text, TouchableOpacity, Linking } from 'react-native'
+import { View, Text, TouchableOpacity, Linking, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
 const GroupsScreen = () => {
   return (
-    <SafeAreaView className="bg-white flex-1 items-center space-y-10 ">
+    <SafeAreaView className="bg-white flex-1">
 
-        <TouchableOpacity className="flex justify-center bg-blue-500 active:bg-violet-700 font-bold rounded h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
-          <Text className = "text-white text-2xl text-center">Food Sharing</Text>
-        </TouchableOpacity>
-
-        <View className = "">
-          <TouchableOpacity className="flex justify-center bg-blue-500 active:bg-violet-700 font-bold rounded h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
-            <Text className = "text-white text-2xl text-center">
-              Car Pooling
-            </Text>
-          </TouchableOpacity>
+        <View>
+          <Text className="text-3xl mt-6 ml-5"> Groups </Text>
         </View>
 
+        <View className="items-center">
+          <View className="mt-11">
+            <TouchableOpacity className="flex justify-center bg-black active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>  
+              <Image className=" bg-white w-10 h-10 absolute ml-8" source={require('../assets/groupsScreen/fast-delivery.png')} />
+              <Text className = "text-white text-2xl text-center ml-12">Food Sharing</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="mt-9">
+            <TouchableOpacity className="flex justify-center bg-black active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
+            <Image className=" bg-white w-10 h-10 absolute ml-10" source={require('../assets/groupsScreen/car-sharing.png')} />
+              <Text className = "text-white text-2xl text-center ml-12">
+                Car Pooling
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
 
     </SafeAreaView>
   )
