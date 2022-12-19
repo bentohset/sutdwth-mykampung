d90ee5ceecff4c08ea7633eabdf464a6718a59f2
Sriram Senthil <sriram.senthilkr@gmail.com>
2022-12-19 18:06:37 +0800

    Added 2 buttons with link

--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Linking } from 'react-native'
 import React from 'react'
 
 const GroupsScreen = () => {
   return (
-    <View>
-      <Text>GroupsScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 items-center space-y-10 ">
+
+        <TouchableOpacity className="flex justify-center bg-blue-500 active:bg-violet-700 font-bold rounded h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
+          <Text className = "text-white text-2xl text-center">Food Sharing</Text>
+        </TouchableOpacity>
+
+        <View className = "">
+          <TouchableOpacity className="flex justify-center bg-blue-500 active:bg-violet-700 font-bold rounded h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
+            <Text className = "text-white text-2xl text-center">
+              Car Pooling
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+
+    </SafeAreaView>
   )
 }
 
