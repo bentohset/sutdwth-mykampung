c25f483cc063c3877a408beb509b3a0b88267754
Benjamin Toh <toh.benjamin123@gmail.com>
2022-12-20 14:45:06 +0800

    profile styling

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TouchableOpacity, Touchable, TouchableHighlight, TextInput, StyleSheet, textAlign} from 'react-native'
+import { View, Text, SafeAreaView, Image, TouchableOpacity, Touchable, TouchableHighlight, TextInput, StyleSheet, textAlign, ScrollView} from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { getDoc, doc, updateDoc, setDoc, addDoc } from "firebase/firestore";
@@ -98,90 +98,90 @@ const ProfileScreen = () => {
     <SafeAreaView className = "flex-1 relative"> 
 
         {/*Profile Section*/} 
-        <View className = "top-12 flex-row items-center justify-center">
-            <Text className = "text-[30px] font-semibold ">
+        <View className = "flex-row items-center justify-between p-2 mr-2 ml-2">
+            <Text className = "text-3xl font-bold ">
                 Profile
             </Text>
+        
+
+            {/*Settings button*/}
+            <TouchableOpacity onPress={() => navigation.navigate('SettingsScreen')}
+                className="flex"
+            >
+                <Image 
+                source = {{
+                    uri: "https://img.icons8.com/dusk/512/gear.png"
+                }}
+                className = "w-14 h-14 object-cover"
+                />
+            </TouchableOpacity>
         </View>
 
-        {/*Settings button*/}
-        <TouchableOpacity onPress={() => navigation.navigate('SettingsScreen')}>
-            <Image 
-            source = {{
-                uri: "https://img.icons8.com/dusk/512/gear.png"
-            }}
-            className = "top-1 left-80 w-14 h-14 object-cover"
-            />
-        </TouchableOpacity>
-
         {/*Profile Image*/}
-        <View className = "items-center">
-            <View className = "top-5 w-40 h-40 bg-red-300 rounded-full items-center justify-center">
+        <View className = "flex items-center">
+            <View className = "w-40 h-40 bg-red-300 rounded-full items-center justify-center">
                 <Image
                 // source= {ProfileImage}
                     className = "w-full h-full rounded-full object-cover shadow-lg"
                 />
             </View>
-        </View>
+        
 
-        {/*Edit Profile Section*/}
-        <TouchableOpacity>
-            <View className = "flex-row item-center justify-center top-8">
-                <Text className = "text-[16px] text-blue-600 font-semibold">
-                    Edit Profile Image
-                </Text>
-            </View>
-        </TouchableOpacity>
+            {/*Edit Profile Section*/}
+            <TouchableOpacity>
+                <View className = "mt-2 item-center justify-center">
+                    <Text className = "text-[16px] text-blue-600 font-semibold">
+                        Edit Profile Image
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+        <ScrollView
+            keyboardDismissMode="on-drag"
+        >
         {/*Edit Name*/}
-        <View >
-            <View>
-                <Text className = "top-16 left-10 font-light text-[14px]">
-                    Name* 
-                </Text>
-            </View>
-            
+        <View className="flex-1 flex-col items-start p-2 pr-8 m-1 mt-0">
+            <Text className="font-semibold m-1 text-xl">
+                Name* 
+            </Text>
             <TextInput 
-                className = "top-16 mx-8 rounded-xl bg-red-50 py-2 px-2"       
+                className="bg-[#D9D9D9] w-full h-12 m-2 p-4 rounded-xl"     
                 onChangeText = {(name) => setName(name)}
                 value = {name}
                 placeholder= {Name}
-            
-                />
-        </View>
+                placeholderTextColor="gray"
+            />
 
         {/*Edit Mobile Number*/}
-        <View>
-            <View>
-                <Text className = "top-20 left-10 font-light text-[14px]">
-                    Mobile Number*
-                </Text>
-            </View>
+            <Text className="font-semibold m-1 text-xl">
+                Mobile Number*
+            </Text>
             <TextInput 
-                className = "relative top-20 items-center mx-8 rounded-xl bg-red-50 py-2 px-2 shadow-lg"
+                className="bg-[#D9D9D9] w-full h-12 m-2 p-4 rounded-xl" 
                 onChangeText = {(number) => setNumber(number)}
                 value = {number}
                 placeholder= {Number}
+                placeholderTextColor="gray"
             />
-        </View>
 
         {/*Edit Profile Description*/}
-        <View> 
-            <Text className = "top-24 left-10 font-light text-[14px]">
+            <Text className="font-semibold m-1 text-xl">
                 Description (Max: 100 Characters)
             </Text>
             <TextInput 
-                className = "top-24 items-center mx-8 rounded-xl bg-red-50 py-6 px-6 shadow-lg text-[12px]" 
+                className="bg-[#D9D9D9] w-full h-12 m-2 p-4 rounded-xl" 
                 maxLength = {100}
                 onChangeText = {(desc) => setDesc(desc)}
                 value = {desc}
                 multiline = {true}
                 placeholder= {Desc}
+                placeholderTextColor="gray"
             />
         </View>
-    
-        <View className = "top-64 items-center justify-center">
+        </ScrollView>
+        <View className = "flex-1 items-center justify-center">
             {/*Edit Cancel Button*/}
-            <TouchableOpacity className = " flex items-center justify-center right-24 w-36 h-12 bg-red-50 rounded-xl mx-8 shadow-lg">
+            <TouchableOpacity className = " flex items-center justify-center right-24 w-36 h-12 bg-[#D9D9D9] rounded-xl mx-8">
                 <Text>
                     Cancel
                 </Text>
@@ -189,22 +189,19 @@ const ProfileScreen = () => {
 
         {/*Edit Save Button*/}
             <TouchableOpacity 
-            onPress={()=>{
-        changeName();
-        changeMobile();
-        changeDesc();
-        setDesc(null);
-        setName(null);
-        setNumber(null);
-        
-            }}
-            className = "flex items-center justify-center left-24 bottom-12 w-36 h-12 bg-blue-400 rounded-xl mx-8 shadow-lg">
+                onPress={()=>{
+                    changeName();
+                    changeMobile();
+                    changeDesc();
+                }}
+                className = "flex items-center justify-center left-24 bottom-12 w-36 h-12 bg-blue-400 rounded-xl mx-8"
+            >
                 <Text>
                     Save
                 </Text>
             </TouchableOpacity>
         </View>
-
+        
     </SafeAreaView>
 
   )
