d541db84430c1616ccb4ed92b4fea1565405fc47
Benjamin Toh <toh.benjamin123@gmail.com>
2022-12-20 14:50:28 +0800

    tinyurl

--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -27,14 +27,14 @@ const GroupsScreen = () => {
 
         <View className="items-center">
           <View className="mt-11">
-            <TouchableOpacity className="flex justify-center bg-[#5b5b5b] active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>  
+            <TouchableOpacity className="flex justify-center bg-[#5b5b5b] active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://tinyurl.com/" + postal)}>  
               <Image className=" bg-white w-10 h-10 absolute ml-8" source={require('../assets/groupsScreen/fast-delivery.png')} />
               <Text className = "text-white text-2xl text-center ml-12">Food Sharing </Text>
             </TouchableOpacity>
           </View>
 
           <View className="mt-9">
-            <TouchableOpacity className="flex justify-center bg-[#5b5b5b] active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://chat.whatsapp.com/E5JZ1fmL2W45IRXKOCT2Mr")}>
+            <TouchableOpacity className="flex justify-center bg-[#5b5b5b] active:bg-zinc-700 font-bold rounded-2xl h-28 w-60" onPress={() => Linking.openURL("https://tinyurl.com/carpl" + postal)}>
             <Image className=" bg-white w-10 h-10 absolute ml-10" source={require('../assets/groupsScreen/car-sharing.png')} />
               <Text className = "text-white text-2xl text-center ml-12">
                 Car Pooling
